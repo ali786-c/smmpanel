04bb0ad815e3378b486718bbfb1a010108dde59e
Muhammad Aliyan <aliyantarar4@gmail.com>
2026-04-19 22:28:44 +0500

    fix: Match PayHub config keys with .env (PAYHUB_CLIENT_SECRET)

--- cpanel-deployment/api/config/services.php
+++ cpanel-deployment/api/config/services.php
@@ -50,7 +50,7 @@ return [
 
     'payhub' => [
         'client_id' => env('PAYHUB_CLIENT_ID'),
-        'secret' => env('PAYHUB_SECRET'),
+        'secret' => env('PAYHUB_CLIENT_SECRET'),
         'url' => env('PAYHUB_API_URL', 'http://linkpaypro.online/api'),
         'success_url' => env('PAYHUB_SUCCESS_URL'),
         'cancel_url' => env('PAYHUB_CANCEL_URL'),
